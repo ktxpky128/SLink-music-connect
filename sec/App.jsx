5ce46629d0866706565b486d60a213fde022a936
ktxpky128 <qfdq5zss96@privaterelay.appleid.com>
2025-11-06 16:59:27 +0800

    Refactor routing in App component

--- sec/App.jsx
+++ sec/App.jsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Nav from './components/Nav'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import Connect from './pages/Connect'
-import Chat from './pages/Chat'
-import SharedRoom from './pages/SharedRoom'
-import NotFound from './pages/NotFound'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Connect from "./pages/Connect";
+import Chat from "./pages/Chat";
+import SharedRoom from "./pages/SharedRoom";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
-    <div className="app">
+    <Router>
       <Nav />
       <main className="container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/connect" element={<Connect />} />
-          <Route path="/chat/:id" element={<Chat />} />
-          <Route path="/room/:id" element={<SharedRoom />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/sharedroom" element={<SharedRoom />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </main>
-    </div>
-  )
+      </Router>
+  );
 }
